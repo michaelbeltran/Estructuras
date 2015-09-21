94f3c77b110c5096a39dd4acab05f8d7dae72e7d
michael <henkel145@gmail.com>
2015-09-21 10:34:55 -0500

    Estudiante.h Estudiante.cpp
    Clase principal para el proyecto

---
+++ Estudiante.h
@@ -0,0 +1,17 @@
+#ifndef _ESTUDIANTE_
+#define _ESTUDIANTE_
+class Estudiante{
+	long int codigo;
+	char *nombre:
+	char *carrera;
+	short int edad;
+	unsigned int dia;
+	unsigned int mes;
+	unsigned int anio;
+	float promedio;
+	Estudiante *sig;
+	Estudiante *ant;
+	public:
+		//
+};
+#endif
